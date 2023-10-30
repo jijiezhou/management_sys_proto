744f737d48ec201d6446b47ddaafa435fe31a1db
jijiezhou <jijiez@email.unc.edu>
2023-10-29 20:39:52 -0400

    14 upload file

--- springboot/src/main/java/com/example/springboot/controller/FileController.java
+++ springboot/src/main/java/com/example/springboot/controller/FileController.java
@@ -27,31 +27,35 @@ public class FileController {
     @Value("${server.port}")
     String port;
 
-    private static final String ROOT_PATH =  System.getProperty("user.dir") + File.separator + "files";  // D:\B站\小白做毕设2024\代码\小白做毕设2024\files
+    private static final String ROOT_PATH =  System.getProperty("user.dir") + File.separator + "files";  // ~\project\honey2024-master\files
 
     @HoneyLogs(operation = "文件", type = LogType.ADD)
     @PostMapping("/upload")
     public Result upload(MultipartFile file) throws IOException {
-        String originalFilename = file.getOriginalFilename();  // 文件的原始名称
+        String originalFilename = file.getOriginalFilename();  // original name of file
         // aaa.png
-        String mainName = FileUtil.mainName(originalFilename);  // aaa
-        String extName = FileUtil.extName(originalFilename);// png
+        String mainName = FileUtil.mainName(originalFilename);  // aaa -> not include postfix
+        String extName = FileUtil.extName(originalFilename);// png -> postfix of file
         if (!FileUtil.exist(ROOT_PATH)) {
-            FileUtil.mkdir(ROOT_PATH);  // 如果当前文件的父级目录不存在，就创建
+            FileUtil.mkdir(ROOT_PATH);  //If the parent directory of the current file does not exist, create it
         }
-        if (FileUtil.exist(ROOT_PATH + File.separator + originalFilename)) {  // 如果当前上传的文件已经存在了，那么这个时候我就要重名一个文件名称
+        //If the currently uploaded file already exists, then I will rename the file at this time using time created
+        if (FileUtil.exist(ROOT_PATH + File.separator + originalFilename)) {
             originalFilename = System.currentTimeMillis() + "_" + mainName + "." + extName;
         }
         File saveFile = new File(ROOT_PATH + File.separator + originalFilename);
-        file.transferTo(saveFile);  // 存储文件到本地的磁盘里面去
+        file.transferTo(saveFile);  //Store files to local disk
+        //http://localhost:9090/file/download/file_name
         String url = "http://" + ip + ":" + port + "/file/download/" + originalFilename;
-        return Result.success(url);  //返回文件的链接，这个链接就是文件的下载地址，这个下载地址就是我的后台提供出来的
+        //Returns the link to the file. This link is the download address of the file. This download address is provided by my backend.
+        return Result.success(url);
     }
 
     @AuthAccess
     @GetMapping("/download/{fileName}")
+    //input: fileName, response
     public void download(@PathVariable String fileName, HttpServletResponse response) throws IOException {
-//        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));  // 附件下载
+//      response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));  // 附件下载
         response.addHeader("Content-Disposition", "inline;filename=" + URLEncoder.encode(fileName, "UTF-8"));  // 预览
         String filePath = ROOT_PATH  + File.separator + fileName;
         if (!FileUtil.exist(filePath)) {
@@ -59,7 +63,7 @@ public class FileController {
         }
         byte[] bytes = FileUtil.readBytes(filePath);
         ServletOutputStream outputStream = response.getOutputStream();
-        outputStream.write(bytes);  // 数组是一个字节数组，也就是文件的字节流数组
+        outputStream.write(bytes);  // array is byte array, which is the byte stream array of the file
         outputStream.flush();
         outputStream.close();
     }

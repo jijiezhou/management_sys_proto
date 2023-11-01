dcd141b4d7125066c4edd260fa50cf9a7eb3a426
jijiezhou <jijiez@email.unc.edu>
2023-10-31 23:54:50 -0400

    Login.vue form to English

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -6,16 +6,16 @@
       </div>
       <div style="flex: 1; display: flex; align-items: center; justify-content: center">
         <el-form :model="user" style="width: 80%" :rules="rules" ref="loginRef">
-          <div style="font-size: 20px; font-weight: bold; text-align: center; margin-bottom: 20px">欢迎登录后台管理系统</div>
+          <div style="font-size: 20px; font-weight: bold; text-align: center; margin-bottom: 20px">Welcome Management System</div>
           <el-form-item prop="username">
-            <el-input prefix-icon="el-icon-user" size="medium" placeholder="请输入账号" v-model="user.username"></el-input>
+            <el-input prefix-icon="el-icon-user" size="medium" placeholder="please enter username" v-model="user.username"></el-input>
           </el-form-item>
           <el-form-item prop="password">
-            <el-input prefix-icon="el-icon-lock" size="medium" show-password placeholder="请输入密码" v-model="user.password"></el-input>
+            <el-input prefix-icon="el-icon-lock" size="medium" show-password placeholder="please enter password" v-model="user.password"></el-input>
           </el-form-item>
           <el-form-item prop="code">
             <div style="display: flex">
-              <el-input placeholder="请输入验证码" prefix-icon="el-icon-circle-check" size="medium" style="flex: 1" v-model="user.code"></el-input>
+              <el-input placeholder="please enter validate code" prefix-icon="el-icon-circle-check" size="medium" style="flex: 1" v-model="user.code"></el-input>
               <div style="flex: 1; height: 36px">
                 <valid-code @update:value="getCode" />
               </div>
@@ -25,7 +25,7 @@
             <el-button type="primary" style="width: 100%" @click="login">Log In</el-button>
           </el-form-item>
           <div style="display: flex">
-            <div style="flex: 1">No Account?<span style="color: #0f9876; cursor: pointer" @click="$router.push('/register')">Register</span></div>
+            <div style="flex: 1">No Account? <span style="color: #0f9876; cursor: pointer" @click="$router.push('/register')">Register</span></div>
             <div style="flex: 1; text-align: right"><span style="color: #0f9876; cursor: pointer" @click="handleForgetPass">Forget Password</span></div>
           </div>
         </el-form>

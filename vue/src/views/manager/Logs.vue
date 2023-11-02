17cb0a298d56e439c5498eb2ce291f20c17fad1f
jijiezhou <jijiez@email.unc.edu>
2023-11-01 23:03:53 -0400

    Logs.vue

--- vue/src/views/manager/Logs.vue
+++ vue/src/views/manager/Logs.vue
@@ -56,7 +56,7 @@ export default {
   name: "Logs",
   data() {
     return {
-      tableData: [],  // 所有的数据
+      tableData: [],  // all data
       pageNum: 1,   // 当前的页码
       pageSize: 5,  // 每页显示的个数
       operation: '',

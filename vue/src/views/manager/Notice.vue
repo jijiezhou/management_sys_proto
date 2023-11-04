52308aad9817b89fb57b493813a416414a5e4c2e
jijiezhou <jijiez@email.unc.edu>
2023-11-03 23:58:13 -0400

    20 Notice.vue middle point

--- vue/src/views/manager/Notice.vue
+++ vue/src/views/manager/Notice.vue
@@ -1,30 +1,33 @@
 <template>
   <div>
     <div>
-      <el-input style="width: 200px" placeholder="查询标题" v-model="title"></el-input>
-      <el-button type="primary" style="margin-left: 10px" @click="load(1)">查询</el-button>
-      <el-button type="info" @click="reset">重置</el-button>
+      <el-input style="width: 200px" placeholder="Query Title" v-model="title"></el-input>
+      <el-button type="primary" style="margin-left: 10px" @click="load(1)">Query</el-button>
+      <el-button type="info" @click="reset">Reset</el-button>
     </div>
+
     <div style="margin: 10px 0">
-      <el-button type="primary" plain @click="handleAdd">新增</el-button>
-      <el-button type="danger" plain @click="delBatch">批量删除</el-button>
+      <el-button type="primary" plain @click="handleAdd">Add</el-button>
+      <el-button type="danger" plain @click="delBatch">DeleteBatch</el-button>
     </div>
-    <el-table :data="tableData" stripe :header-cell-style="{ backgroundColor: 'aliceblue', color: '#666' }" @selection-change="handleSelectionChange">
+
+    <el-table :data="tableData" stripe :header-cell-style="{ backgroundColor: 'aliceblue', color: '#666' }"
+              @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" align="center"></el-table-column>
-      <el-table-column prop="id" label="序号" width="70" align="center"></el-table-column>
-      <el-table-column prop="title" label="标题"></el-table-column>
-      <el-table-column prop="content" label="内容" show-overflow-tooltip></el-table-column>
-      <el-table-column prop="user" label="发布人"></el-table-column>
-      <el-table-column prop="time" label="发布时间"></el-table-column>
-      <el-table-column label="是否公开">
+      <el-table-column prop="id" label="id" width="70" align="center"></el-table-column>
+      <el-table-column prop="title" label="title"></el-table-column>
+      <el-table-column prop="content" label="content" show-overflow-tooltip></el-table-column>
+      <el-table-column prop="user" label="poster"></el-table-column>
+      <el-table-column prop="time" label="time"></el-table-column>
+      <el-table-column label="isOpen">
         <template v-slot="scope">
           <el-switch v-model="scope.row.open" @change="changeOpen(scope.row)"></el-switch>
         </template>
       </el-table-column>
-      <el-table-column label="操作" align="center" width="180">
+      <el-table-column label="operate" align="center" width="180">
         <template v-slot="scope">
-          <el-button size="mini" type="primary" plain @click="handleEdit(scope.row)">编辑</el-button>
-          <el-button size="mini" type="danger" plain @click="del(scope.row.id)">删除</el-button>
+          <el-button size="mini" type="primary" plain @click="handleEdit(scope.row)">Edit</el-button>
+          <el-button size="mini" type="danger" plain @click="del(scope.row.id)">Delete</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -39,19 +42,19 @@
       </el-pagination>
     </div>
 
-    <el-dialog title="公告信息" :visible.sync="fromVisible" width="40%" :close-on-click-modal="false">
+    <el-dialog title="Notice Info" :visible.sync="fromVisible" width="40%" :close-on-click-modal="false">
       <el-form :model="form" label-width="80px" style="padding-right: 20px" :rules="rules" ref="formRef">
-        <el-form-item label="标题" prop="title">
-          <el-input v-model="form.title" placeholder="标题"></el-input>
+        <el-form-item label="title" prop="title">
+          <el-input v-model="form.title" placeholder="title"></el-input>
         </el-form-item>
-        <el-form-item label="内容" prop="content">
-          <el-input type="textarea" v-model="form.content" placeholder="内容"></el-input>
+        <el-form-item label="content" prop="content">
+          <el-input type="textarea" v-model="form.content" placeholder="content"></el-input>
         </el-form-item>
       </el-form>
 
       <div slot="footer" class="dialog-footer">
-        <el-button @click="fromVisible = false">取 消</el-button>
-        <el-button type="primary" @click="save">确 定</el-button>
+        <el-button @click="fromVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="save">Confirm</el-button>
       </div>
     </el-dialog>
 
@@ -75,10 +78,10 @@ export default {
       user: JSON.parse(localStorage.getItem('honey-user') || '{}'),
       rules: {
         title: [
-          { required: true, message: '请输入标题', trigger: 'blur' },
+          {required: true, message: '请输入标题', trigger: 'blur'},
         ],
         content: [
-          { required: true, message: '请输入内容', trigger: 'blur' },
+          {required: true, message: '请输入内容', trigger: 'blur'},
         ]
       },
       ids: [],
@@ -89,9 +92,9 @@ export default {
     this.load()
   },
   methods: {
+    // Call the updated interface to update data to the database
     changeOpen(form) {
-      // 调用更新的接口  更新数据到数据库
-      this.form = JSON.parse(JSON.stringify(form))
+      this.form = JSON.parse(JSON.stringify(form)) //deep copy
       this.sendSaveRequest()   // 直接发请求就可以了
     },
     delBatch() {
@@ -100,7 +103,7 @@ export default {
         return
       }
       this.$confirm('您确认批量删除这些数据吗？', '确认删除', {type: "warning"}).then(response => {
-        this.$request.delete('/notice/delete/batch', { data: this.ids }).then(res => {
+        this.$request.delete('/notice/delete/batch', {data: this.ids}).then(res => {
           if (res.code === '200') {   // 表示操作成功
             this.$message.success('操作成功')
             this.load(1)
@@ -108,7 +111,8 @@ export default {
             this.$message.error(res.msg)  // 弹出错误的信息
           }
         })
-      }).catch(() => {})
+      }).catch(() => {
+      })
     },
     handleSelectionChange(rows) {   // 当前选中的所有的行数据
       this.ids = rows.map(v => v.id)
@@ -123,7 +127,8 @@ export default {
             this.$message.error(res.msg)  // 弹出错误的信息
           }
         })
-      }).catch(() => {})
+      }).catch(() => {
+      })
     },
     handleEdit(row) {   // 编辑数据
       this.form = JSON.parse(JSON.stringify(row))  // 给form对象赋值  注意要深拷贝数据
@@ -142,7 +147,7 @@ export default {
     },
     sendSaveRequest() {
       this.$request({
-        url: this.form.id ? '/notice/update': '/notice/add',
+        url: this.form.id ? '/notice/update' : '/notice/add',
         method: this.form.id ? 'PUT' : 'POST',
         data: this.form
       }).then(res => {
@@ -160,7 +165,7 @@ export default {
       this.load()
     },
     load(pageNum) {  // 分页查询
-      if (pageNum)  this.pageNum = pageNum
+      if (pageNum) this.pageNum = pageNum
       this.$request.get('/notice/selectByPage', {
         params: {
           pageNum: this.pageNum,
@@ -180,5 +185,7 @@ export default {
 </script>
 
 <style>
-.el-tooltip__popper{ max-width:300px !important; }
+.el-tooltip__popper {
+  max-width: 300px !important;
+}
 </style>
